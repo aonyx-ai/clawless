8afd54a6cb8eed8b3008b81fee2825f44af2974b
Jan David <jdno@jdno.dev>
2025-06-02 21:05:03 +0200

    Switch CLI to clap's builder pattern

--- crates/clawless-cli/src/main.rs
+++ crates/clawless-cli/src/main.rs
@@ -1,5 +1,26 @@
-use clawless::clawless;
+use clap::{Args, Command, FromArgMatches};
+
+use crate::commands::new::NewArgs;
 
 mod commands;
 
-clawless!();
+#[tokio::main]
+async fn main() {
+    let new = NewArgs::augment_args(Command::new("new"));
+
+    let app = Command::new("clawless")
+        .version(env!("CARGO_PKG_VERSION"))
+        .about("A framework for building command-line applications in Rust")
+        .arg_required_else_help(true)
+        .subcommand(new);
+
+    let matches = app.get_matches();
+
+    match matches.subcommand() {
+        Some(("new", matches)) => {
+            let args = NewArgs::from_arg_matches(matches).unwrap();
+            commands::new::run(&args).await;
+        }
+        _ => {}
+    }
+}
